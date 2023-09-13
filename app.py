233e1ee9be0dcadf1b46deba2fbae028b33ac5dd
Донельчук Артём <mega.donelchuk@mail.ru>
2023-09-13 18:49:42 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
-    
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Донельчук Артём Вячеславович, лабораторная работа 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Донельчук Артём, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""

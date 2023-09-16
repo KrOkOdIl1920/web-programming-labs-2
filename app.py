352f9bce03000b6fec2144e2f2b03bb6675602ea
Донельчук Артём <mega.donelchuk@mail.ru>
2023-09-16 11:46:18 +0700

    Меню

--- app.py
+++ app.py
@@ -2,7 +2,36 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def start():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ ФБ Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <main>
+
+        <ol>
+            <li><a href="http://127.0.0.1:5000/lab1" target="_blank">Первая лабораторная</a></li>
+        <ol>
+
+        </main>
+
+        <footer>
+            &copy; Донельчук Артём, ФБИ-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/lab1")
+def lab1():
     return """
 <!doctype html>
 <html>
@@ -15,6 +44,13 @@ def start():
         </header>
 
         <h1>web-сервер на flask</h1>
+        <p>
+            Flask — фреймворк для создания веб-приложений на языке
+              программирования Python, использующий набор инструментов Werkzeug,
+                а также шаблонизатор Jinja2. Относится к категории так называемых
+                  микрофреймворков — минималистичных каркасов веб-приложений,
+                    сознательно предоставляющих лишь самые базовые возможности.
+        </p>
 
         <footer>
             &copy; Донельчук Артём, ФБИ-13, 3 курс, 2023

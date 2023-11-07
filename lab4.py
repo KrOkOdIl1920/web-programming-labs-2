f6a463984fe7f723611175bec837556995ca06fa
Донельчук Артём <mega.donelchuk@mail.ru>
2023-11-08 02:48:37 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -5,3 +5,28 @@ lab4 = Blueprint("lab4", __name__)
 @lab4.route("/lab4/")
 def lab():
     return render_template("lab4.html")
+
+
+@lab4.route("/lab4/login/", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        print(request.method)
+        return render_template("login.html")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username == "alex" and password == "123":
+        return render_template("success4.html", username=username)
+
+    if not username and not password:
+        error = "Не введен логин и пароль!"
+        return render_template("login.html", error=error, username=username, password=password)
+    elif not password:
+        error = "Не введён пароль!"
+        return render_template("login.html", error=error, username=username, password=password)
+    elif not username:
+        error = "Не введён логин!"
+        return render_template("login.html", error=error, username=username, password=password)
+    else:
+        error = "Неверный логин и/или пароль!"
+        return render_template("login.html", error=error, username=username, password=password)
